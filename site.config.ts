c6777b3abc3946612132fdd4322edcb4d3042310
yyysl <352848845@qq.com>
2023-03-18 23:59:34 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -18,9 +18,6 @@ export default siteConfig({
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
